0238fc59d841c96fa1cfe08ec35f74086bebbd52
John D Allen <john.allen@leveridgesystems.com>
2021-05-19 09:18:37 -0500

    IERC165 interface added

--- runitnft.sol
+++ runitnft.sol
@@ -100,6 +100,30 @@ abstract contract Ownable is Context {
 
 
 
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC165 standard, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-165[EIP].
+ *
+ * Implementers can declare support of contract interfaces, which can then be
+ * queried by others ({ERC165Checker}).
+ *
+ * For an implementation, see {ERC165}.
+ */
+interface IERC165 {
+    /**
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[EIP section]
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30 000 gas.
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+
 
 
 pragma solidity ^0.8.0;
@@ -108,6 +132,8 @@ pragma solidity ^0.8.0;
 /**
  * @dev Required interface of an ERC721 compliant contract.
  */
+
+
 interface IERC721 is IERC165 {
     /**
      * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
@@ -228,6 +254,7 @@ interface IERC721 is IERC165 {
       */
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
 }
+
 
 
 
@@ -547,8 +574,6 @@ library Strings {
 
 
 
-pragma solidity ^0.8.0;
-
 
 /**
  * @dev Implementation of the {IERC165} interface.
@@ -564,6 +589,9 @@ pragma solidity ^0.8.0;
  *
  * Alternatively, {ERC165Storage} provides an easier to use but more expensive implementation.
  */
+
+pragma solidity ^0.8.0;
+
 abstract contract ERC165 is IERC165 {
     /**
      * @dev See {IERC165-supportsInterface}.
